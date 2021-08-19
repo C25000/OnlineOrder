83f52635fe24854364888d4312ac9d65de822f94
zchang <csuchangzheng@gmail.com>
2021-08-18 23:03:06 -0500

    change login method

--- src/main/java/backend/onlineOrder/ApplicationConfig.java
+++ src/main/java/backend/onlineOrder/ApplicationConfig.java
@@ -18,30 +18,22 @@ public class ApplicationConfig {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.laioffer.onlineOrder.entity");
+        sessionFactory.setPackagesToScan("backend.onlineOrder.entity");
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
-
     @Bean(name = "dataSource")
-    public DataSource dataSource() throws IOException {
-        // put username and password in config file. Read config file from here
-        Properties prop = new Properties();
-        String propFileName = "config.properties";
-        InputStream inputStream = ApplicationConfig.class.getClassLoader().getResourceAsStream(propFileName);
-        prop.load(inputStream);
-
-        String username = prop.getProperty("user");
-        String password = prop.getProperty("password");
-
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://laiproject-instance.cbbya17btbu4.us-east-2.rds.amazonaws.com:3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        dataSource.setUsername("");
+        dataSource.setPassword("");
 
         return dataSource;
     }
+
+
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
@@ -50,3 +42,33 @@ public class ApplicationConfig {
         return hibernateProperties;
     }
 }
+
+
+
+//    @Bean(name = "dataSource")
+//    public DataSource dataSource() throws IOException {
+//        // put username and password in config file. Read config file from here
+//        Properties prop = new Properties();
+//        String propFileName = "config.properties";
+//        InputStream inputStream = ApplicationConfig.class.getClassLoader().getResourceAsStream(propFileName);
+//        prop.load(inputStream);
+//
+//        String username = prop.getProperty("user");
+//        String password = prop.getProperty("password");
+//
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://laiproject-instance.cbbya17btbu4.us-east-2.rds.amazonaws.com:3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//
+//        return dataSource;
+//    }
+//    private final Properties hibernateProperties() {
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+//        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+//        hibernateProperties.setProperty("hibernate.show_sql", "true");
+//        return hibernateProperties;
+//    }
+//}
