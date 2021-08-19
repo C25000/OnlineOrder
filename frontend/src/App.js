46bae7ef070b6afbfef3d8a4275769a44a5c5e48
zchang <csuchangzheng@gmail.com>
2021-08-18 23:09:49 -0500

    add frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+
+const { Header, Content } = Layout;
+const { Title } = Typography;
+
+function App() {
+  const [authed, setAuthed] = useState(false);
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px)",
+          overflowY: "auto",
+        }}
+      >
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
+
